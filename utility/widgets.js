ac58f3ed28bd5e6afe36d21b0e32dbb2ff51afde
Darren Dignam <darren.dignam@gmail.com>
2021-10-13 23:51:15 +0200

    ReWrote the smart debit start date utility function to match the 14th or 28th requirement, not just a random 30days ahead of now as it was before

--- utility/widgets.js
+++ utility/widgets.js
@@ -78,21 +78,46 @@ module.exports = {
         },
 
     /**
-     * Using the current date.now + one month!
+     * Using the current date.now. Add 5 days - the min amount needed for a direct debit mandate to process??? (not sure)
+     * Then try and fit that date to either the 14th or 28th of the month? its the day they manually process DD's
      * This helper function will convert the date format to the style used by the Smart Debit System.
+     * 
+     * At least 5 days away. and then either 14 or 28
      */
      smartDebit_formatMonthTodayYYYYMMDD: () => {
         var d = new Date(),
-            month = '' + (d.getMonth() + 2),
-            day = '' + d.getDate(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + (d.getDate()+5),         //the +5 here is adding 5 days to now, the min time needed for buffering the first DD payment for the banks to do their thing?
             year = d.getFullYear();
+
+            //Might noee a second pair of eyes to double check this!!
+            day = parseInt(day);
+            if(day<14){                      //now +5 is less than the 14th, so set it to the 14th
+                day = ''+14;
+            }else if(day<28){                //now +5 is after the 14th but before the 28th, so roll them upto the 28th
+                day = ''+28;
+            }else if(day > 28){              //now +5 is after the 28th, so its next month?
+                day = ''+14;
+                //rollover the month, and perhaps the year!?
+                if( parseInt(month)+1 <= 12 ){
+                    month = ''+(parseInt(month)+1);
+                }else{
+                    month = ''+1; //jan
+                    year = ''+ (parseInt(year)+1); //inc year
+                }
+            }else{                           //not sure what the date is now sigh!! HOW?
+                day = ''+28;
+            }
+
 
         if (month.length < 2)
             month = '0' + month;
         if (day.length < 2)
             day = '0' + day;
 
-        return [year, month, day].join('-');
+        let _dd_date = [year, month, day].join('-');
+        console.log('DD DATE: ' + _dd_date);
+        return _dd_date;
     },
 
 };
